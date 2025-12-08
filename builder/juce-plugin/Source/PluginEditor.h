081d130a68fca4a598ce8b598cf82bbd4730630b
robertcossey19 <robertallencossey@gmail.com>
2025-12-08 12:56:30 -0800

    PluginEditor.h

--- builder/juce-plugin/Source/PluginEditor.h
+++ builder/juce-plugin/Source/PluginEditor.h
@@ -1,16 +1,20 @@
 #pragma once
 
-#include <juce_gui_extra/juce_gui_extra.h>
+#include <juce_audio_processors/juce_audio_processors.h>
 
 class HtmlToVstAudioProcessor;
 
+/**
+ * Basic JUCE editor for the HtmlToVst plugin.
+ * For now this is just a placeholder with a black background and some text.
+ */
 class HtmlToVstAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
     HtmlToVstAudioProcessorEditor (HtmlToVstAudioProcessor&);
-    ~HtmlToVstAudioProcessorEditor() override = default;
+    ~HtmlToVstAudioProcessorEditor() override;
 
-    void paint (juce::Graphics&) override;
+    void paint (juce::Graphics& g) override;
     void resized() override;
 
 private:
