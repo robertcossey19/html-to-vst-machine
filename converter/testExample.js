70e31bc347de45e00d43febad6d468619b86e6b2
robertcossey19 <robertallencossey@gmail.com>
2025-12-08 11:09:26 -0800

    Create testExample.js

---
+++ converter/testExample.js
@@ -0,0 +1,8 @@
+html-to-vst-machine/
+  spec/
+    plugin-format.md          ← defines the language
+  examples/
+    example-plugin.html       ← example CodePen-style plugin
+  converter/
+    parsePluginSpec.js        ← brain: text → spec
+    testExample.js            ← test: feed example into brain
