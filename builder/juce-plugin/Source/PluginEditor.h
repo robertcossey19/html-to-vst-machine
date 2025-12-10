9606f367b5219660b5fc195fda31c7459ee4c10e
robertcossey19 <robertallencossey@gmail.com>
2025-12-10 15:31:13 -0800

    PluginEditor.h

--- builder/juce-plugin/Source/PluginEditor.h
+++ builder/juce-plugin/Source/PluginEditor.h
@@ -3,20 +3,24 @@
 #include <juce_gui_extra/juce_gui_extra.h>
 #include "PluginProcessor.h"
 
+// Simple editor that shows the embedded HTML UI using WebBrowserComponent.
+// HTML is loaded from JUCE BinaryData and written to a temp file.
+// The WebBrowser then navigates to that file URL so Cubase renders it as real HTML.
+
 class HtmlToVstAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
     HtmlToVstAudioProcessorEditor (HtmlToVstAudioProcessor&);
     ~HtmlToVstAudioProcessorEditor() override;
 
-    void paint (juce::Graphics&) override;
+    void paint (juce::Graphics& g) override;
     void resized() override;
 
 private:
-    HtmlToVstAudioProcessor& audioProcessor;
-
-    // The embedded browser that will render your HTML UI
+    HtmlToVstAudioProcessor& processor;
     juce::WebBrowserComponent webView;
+
+    void loadEmbeddedHtml();   // load HTML from BinaryData and show it
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (HtmlToVstAudioProcessorEditor)
 };
