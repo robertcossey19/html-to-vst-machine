e59c8e733bb0d11968ebd06438c985131cdca805
robertcossey19 <robertallencossey@gmail.com>
2025-12-11 10:40:11 -0800

    PluginEditor.h

--- builder/juce-plugin/Source/PluginEditor.h
+++ builder/juce-plugin/Source/PluginEditor.h
@@ -3,8 +3,11 @@
 #include <juce_gui_extra/juce_gui_extra.h>
 #include "PluginProcessor.h"
 
-class HtmlToVstAudioProcessorEditor  : public juce::AudioProcessorEditor,
-                                       private juce::Timer
+// Simple editor: a WebBrowserComponent that renders ampex_ui.html
+// and a timer that pushes VU values into JS.
+class HtmlToVstAudioProcessorEditor
+    : public juce::AudioProcessorEditor,
+      private juce::Timer
 {
 public:
     explicit HtmlToVstAudioProcessorEditor (HtmlToVstAudioProcessor&);
