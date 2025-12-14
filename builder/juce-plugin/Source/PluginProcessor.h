5328be3ac704ff694731a739d5a3c48254e41372
robertcossey19 <robertallencossey@gmail.com>
2025-12-14 15:37:23 -0800

    PluginProcessor.h

--- builder/juce-plugin/Source/PluginProcessor.h
+++ builder/juce-plugin/Source/PluginProcessor.h
@@ -1,5 +1,16 @@
 #pragma once
-#include <JuceHeader.h>
+
+#include <atomic>
+#include <cmath>
+
+#if __has_include(<JuceHeader.h>)
+  #include <JuceHeader.h>
+#else
+  #include <juce_audio_processors/juce_audio_processors.h>
+  #include <juce_dsp/juce_dsp.h>
+  #include <juce_gui_basics/juce_gui_basics.h>
+  #include <juce_gui_extra/juce_gui_extra.h>
+#endif
 
 class HtmlToVstPluginAudioProcessor : public juce::AudioProcessor
 {
@@ -7,7 +18,6 @@ public:
     HtmlToVstPluginAudioProcessor();
     ~HtmlToVstPluginAudioProcessor() override;
 
-    //==============================================================================
     void prepareToPlay (double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
 
@@ -17,49 +27,43 @@ public:
 
     void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
 
-    //==============================================================================
     juce::AudioProcessorEditor* createEditor() override;
     bool hasEditor() const override;
 
-    //==============================================================================
     const juce::String getName() const override;
-
     bool acceptsMidi() const override;
     bool producesMidi() const override;
     bool isMidiEffect() const override;
     double getTailLengthSeconds() const override;
 
-    //==============================================================================
     int getNumPrograms() override;
     int getCurrentProgram() override;
     void setCurrentProgram (int index) override;
     const juce::String getProgramName (int index) override;
     void changeProgramName (int index, const juce::String& newName) override;
 
-    //==============================================================================
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
-    //==============================================================================
-    juce::AudioProcessorValueTreeState apvts;
-
-    // UI bridge
+    // HTML UI -> DSP bridge (called by editor when JS triggers a juce: URL)
     void handleUiMessageUrl (const juce::String& url);
 
-    // meters (linear 0..1)
-    float getMeterL() const noexcept { return meterL.load(); }
-    float getMeterR() const noexcept { return meterR.load(); }
+    float getOutputMeterL() const noexcept { return meterL.load (std::memory_order_relaxed); }
+    float getOutputMeterR() const noexcept { return meterR.load (std::memory_order_relaxed); }
+
+    juce::AudioProcessorValueTreeState apvts;
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
 
 private:
-    //==============================================================================
-    static juce::AudioProcessorValueTreeState::ParameterLayout createParameterLayout();
+    void updateDspFromParams();
+
+    juce::dsp::Gain<float> inGain;
+    juce::dsp::Gain<float> outGain;
+
+    bool transformerOn = true;
 
     std::atomic<float> meterL { 0.0f };
     std::atomic<float> meterR { 0.0f };
-
-    // smoothed gain
-    juce::SmoothedValue<float, juce::ValueSmoothingTypes::Linear> inGainSm;
-    juce::SmoothedValue<float, juce::ValueSmoothingTypes::Linear> outGainSm;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (HtmlToVstPluginAudioProcessor)
 };
