04769588bb336db4f37674a2f3ba96850286cf7b
robertcossey19 <robertallencossey@gmail.com>
2025-12-15 14:42:42 -0800

    PluginEditor.h

--- builder/juce-plugin/Source/PluginEditor.h
+++ builder/juce-plugin/Source/PluginEditor.h
@@ -1,25 +1,30 @@
 #pragma once
-
-#include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-//==============================================================================
-/**
-*/
-class HtmlToVstPluginAudioProcessorEditor  : public juce::AudioProcessorEditor
+class HtmlToVstPluginAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
     explicit HtmlToVstPluginAudioProcessorEditor (HtmlToVstPluginAudioProcessor&);
     ~HtmlToVstPluginAudioProcessorEditor() override;
 
-    //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
+    // IMPORTANT: don't name this "processor" because AudioProcessorEditor already has a member called processor
     HtmlToVstPluginAudioProcessor& audioProcessor;
+
+    juce::WebBrowserComponent webView;
+
+    juce::File tempHtmlFile;
+    bool uiLoaded = false;
+
+    static juce::String urlDecodeToString (const juce::String& s);
+    static bool looksLikeHtml (const juce::String& s);
+    static juce::String makeMissingUiHtml (const juce::String& extra);
+
+    void writeHtmlToTempAndLoad (const juce::String& html);
+    void loadUiFromBinaryData();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (HtmlToVstPluginAudioProcessorEditor)
 };
