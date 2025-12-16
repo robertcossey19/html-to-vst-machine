05573c03fcebd3aff7bf5f5be60e23f8268da15b
robertcossey19 <robertallencossey@gmail.com>
2025-12-16 12:05:46 -0800

    PluginEditor.h

--- builder/juce-plugin/Source/PluginEditor.h
+++ builder/juce-plugin/Source/PluginEditor.h
@@ -1,13 +1,10 @@
 #pragma once
-
 #include <JuceHeader.h>
-#include "PluginProcessor.h"
 
-//==============================================================================
-// Editor
-class HtmlToVstPluginAudioProcessorEditor final
-    : public juce::AudioProcessorEditor
-    , private juce::Timer
+class HtmlToVstPluginAudioProcessor;
+
+class HtmlToVstPluginAudioProcessorEditor final : public juce::AudioProcessorEditor,
+                                                  private juce::Timer
 {
 public:
     explicit HtmlToVstPluginAudioProcessorEditor (HtmlToVstPluginAudioProcessor&);
@@ -19,12 +16,11 @@ public:
 private:
     void timerCallback() override;
 
-    // Loads index.html from BinaryData and navigates the embedded browser to it.
+    HtmlToVstPluginAudioProcessor& audioProcessor;
+
+    juce::WebBrowserComponent web;
+
     void loadUiFromBinaryData();
-
-    HtmlToVstPluginAudioProcessor& processor;
-
-    juce::WebBrowserComponent webView;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (HtmlToVstPluginAudioProcessorEditor)
 };
