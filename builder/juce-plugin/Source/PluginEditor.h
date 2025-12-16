decb7c3a91a529b4144cc995d856ebf6c6cd546d
robertcossey19 <robertallencossey@gmail.com>
2025-12-16 14:56:54 -0800

    PluginEditor.h

--- builder/juce-plugin/Source/PluginEditor.h
+++ builder/juce-plugin/Source/PluginEditor.h
@@ -1,23 +1,27 @@
 #pragma once
 
-#include <JuceHeader.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+#include <juce_audio_processors/juce_audio_processors.h>
 
-class HtmlToVstPluginAudioProcessor;
+#include "PluginProcessor.h"
 
-class HtmlToVstPluginAudioProcessorEditor  : public juce::AudioProcessorEditor
+class HtmlToVstPluginAudioProcessorEditor final : public juce::AudioProcessorEditor
 {
 public:
     explicit HtmlToVstPluginAudioProcessorEditor (HtmlToVstPluginAudioProcessor&);
-    ~HtmlToVstPluginAudioProcessorEditor() override;
+    ~HtmlToVstPluginAudioProcessorEditor() override = default;
 
     void paint (juce::Graphics&) override;
     void resized() override;
 
 private:
-    HtmlToVstPluginAudioProcessor& audioProcessor;
+    HtmlToVstPluginAudioProcessor& processor;
 
-    // If you want to disable navigation/network, set options as needed.
-    juce::WebBrowserComponent browser;
+    juce::Slider inGain, drive, outGain;
+    juce::Label  inLbl, driveLbl, outLbl;
+
+    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    std::unique_ptr<Attachment> inAtt, driveAtt, outAtt;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (HtmlToVstPluginAudioProcessorEditor)
 };
